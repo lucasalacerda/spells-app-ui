d9f5555cb96ea37d97c5f1f0d4f553ce8b6457c0
Lucas Lacerda <lucasalvlacerda@gmail.com>
2019-11-05 00:56:48 -0300

    add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SpellListComponent } from './spell-list/spell-list.component';
 import { SpellDetailComponent } from './spell-detail/spell-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'spells-list', pathMatch: 'full' },
   { path: 'spell-detail/:id', component: SpellDetailComponent },
   { path: 'spells-list', component: SpellListComponent },
+  { path: 'login', component: LoginComponent },
+
 ];
 
 @NgModule({
